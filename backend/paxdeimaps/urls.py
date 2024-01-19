758fbb28e4ddb77606abd5feaa13947afc99548e
Marcel Walk <marcel.walk@nyasaki.dev>
2024-01-19 22:12:44 +0100

    Added auth endpoint

--- backend/paxdeimaps/urls.py
+++ backend/paxdeimaps/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from paxdeimaps import views
 from rest_framework import routers
+from rest_framework.authtoken import views as authviews
 
 router = routers.DefaultRouter()
 router.register(r'api/resources', views.ResourceViewSet)
@@ -10,7 +11,8 @@ router.register(r'api/categories', views.CategoryViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/auth/', authviews.obtain_auth_token)
 ]
 
 urlpatterns += router.urls
